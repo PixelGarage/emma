9db470cb9256aa2263938c660d026b753d5e0a55
Ralph Moser <ralph@ramosoft.ch>
2016-08-03 23:38:55 +0200

    - postcard image creation finished - submission page styled - db updated

--- all/themes/pixelgarage/templates/webform-submission-page.tpl.php
+++ all/themes/pixelgarage/templates/webform-submission-page.tpl.php
@@ -13,6 +13,56 @@
  * - $submission_navigation: The previous submission ID.
  * - $submission_information: The next submission ID.
  */
+
+//
+// get post node from submission
+$is_postcard_delivery_form = false;
+$img_html = false;
+$master = postcard_webform_master_form($node);
+
+if ($master->nid == 18) {
+  //
+  // POSTCARD DATA webform, get example image from webform
+  $img_uri = !empty($master->field_image) ? $master->field_image[LANGUAGE_NONE][0]['uri'] : false;
+  if ($img_uri) {
+    $img_url = file_create_url($img_uri);
+    $vars = array (
+      'path' => $img_uri,
+      'alt' => 'Model image',
+      'attributes' => array('class'=>array('img-responsive', 'model-image')),
+    );
+    $img_html = theme_image($vars);
+  }
+}
+else if ($master->nid == 789) {
+  //
+  // POSTCARD DELIVERY webform, get postcard image from post
+  $post_nid = false;
+  $is_postcard_delivery_form = true;
+  foreach ($master->webform['components'] as $key => $data) {
+    if ($data['form_key'] == 'post_nid') {
+      $post_nid = $submission->data[$key][0];
+      continue;
+    }
+  }
+
+  //
+  // get postcard image from post node, if available
+  if ($post_nid) {
+    $post = node_load($post_nid);
+    $img_uri = !empty($post->field_hires_image) ? $post->field_hires_image[$post->language][0]['uri'] : false;
+    if ($img_uri) {
+      $img_url = file_create_url($img_uri);
+      $vars = array (
+        'path' => $img_uri,
+        'alt' => 'Postcard image',
+        'attributes' => array('class'=>array('img-responsive', 'postcard-image')),
+      );
+      $img_html = theme_image($vars);
+    }
+  }
+}
+
 ?>
 
 <?php if ($mode == 'display' || $mode == 'form'): ?>
@@ -22,6 +72,22 @@
 <?php endif; ?>
 
 <div class="webform-submission">
-  <?php print render($submission_content); ?>
+  <?php if ($img_html): ?>
+    <div class="row">
+      <?php if ($is_postcard_delivery_form): ?>
+        <div class="col-sm-6"><?php print $img_html; ?></div>
+        <div class="col-sm-6"><?php print render($submission_content); ?></div>
+      <?php else: ?>
+        <div class="col-sm-6"><?php print render($submission_content); ?></div>
+        <div class="col-sm-6"><?php print $img_html; ?></div>
+      <?php endif; ?>
+    </div>
+  <?php else: ?>
+    <div class="row">
+      <div class="col-sm-12">
+        <?php print render($submission_content); ?>
+      </div>
+    <div>
+  <?php endif; ?>
 </div>
 
